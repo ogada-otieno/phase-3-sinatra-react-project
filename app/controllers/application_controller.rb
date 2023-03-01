6be23a38d61b328575830fde3e8da22d9ffd7c4c
Rodgers Ogada <ogadarodgers@gmail.com>
2023-03-01 16:16:29 +0300

    A user should be able to add a new project to their portfolio

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,18 @@ class ApplicationController < Sinatra::Base
     project.to_json
   end
 
+  # # A user should be able to add a new project to their portfolio.
+  post '/create-project/:id' do
+    project = User.find(params[:id]).projects.create(
+      title: params[:title],
+      description: params[:description],
+      image_url: params[:image_url]
+    )
+    project.to_json
+
+  end
+
+  
 
 
 
